609b582223b7ef451fbe94ab37505c8a692bb66a
priyam930 <priyamsanodiya340@gmail.com>
2024-12-22 21:30:52 +0530

    new feature

--- check_memory_usage_percentage/check_mem_usage.sh
+++ check_memory_usage_percentage/check_mem_usage.sh
@@ -1,9 +1,17 @@
-total_mem=`free -m | head -2 | tail -1 | awk '{print $2}'`
-
-used_mem=`free -m | head -2 | tail -1 | awk '{print $3}'`
-
-mem_used_percentage=`expr $used_mem \* 100 / $total_mem` 
-
-
-echo "`date +"%r %D"`   $mem_used_percentage" >> Data.tsv
-
+while :
+do
+	total_mem=`free -m | head -2 | tail -1 | awk '{print $2}'`
+	
+	used_mem=`free -m | head -2 | tail -1 | awk '{print $3}'`
+	
+	mem_used_percentage=`expr $used_mem \* 100 / $total_mem` 
+	
+	echo "`date +"%r %D"`   $mem_used_percentage" >> Data.tsv
+	
+	if [[ $mem_used_percentage -ge 80 ]]
+	then
+		echo "security alert for memory " | espeak-ng
+		exit 	
+	fi
+	sleep 10
+done
